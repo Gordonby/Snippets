c657bebdcddd14b772bc317e8f11b891b7b48ece
Gordon Byers <gordonbyers@gmail.com>
2016-10-03 14:43:07 +0100

    Resets all SQL databases in subscription to a specific tier

---
+++ Reset-AzureSqlDatabaseTier.ps1
@@ -0,0 +1,9 @@
+﻿#gordon.byers@microsoft.com
+#Powershell script is provided as-is and without any warranty of any kind
+
+#Login to Azure
+Login-AzureRmAccount
+
+#Iterate through all databases in the Subscription where Editon is not basic.
+$desiredDbTier = "Basic"
+Get-AzureRmResourceGroup | % {Get-AzureRmSqlServer -ResourceGroupName $_.ResourceGroupName | % {Get-AzureRmSqlDatabase -ServerName $_.Servername -ResourceGroupName $_.ResourceGroupName | ? {$_.CurrentServiceObjectiveName -ne "System" -and $_.CurrentServiceObjectiveName -ne $desiredDbTier -and $_.DatabaseName -ne "Master" } | Set-AzureRmSqlDatabase -Edition $desiredDbTier }}
