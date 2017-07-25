84aaac7c714a8226ae6c0e8bd86b48bac39ab7f5
Gordon Byers <gordonbyers@gmail.com>
2017-07-25 11:33:13 +0100

    Added nextlink pagination support
    As proposed by lesterw1 in the commit comments.  https://github.com/lesterw1

--- Get-AzureBillingUsage.ps1
+++ Get-AzureBillingUsage.ps1
@@ -1,4 +1,4 @@
-﻿#Get price sheet and usage details for the current period.
+#Get price sheet and usage details for the current period.
 #https://docs.microsoft.com/en-us/azure/billing/billing-enterprise-api
 
 
@@ -34,16 +34,20 @@ function Get-AzureBillUsage
     $authHeaders = @{"authorization"="bearer $accesskey"}
     $usageUrl = "https://consumption.azure.com/v1/enrollments/$enrollmentNo/usagedetails"
 
-    $uResponse = Invoke-WebRequest $usageUrl -Headers $authHeaders
-    if ($uResponse.StatusCode -eq 200) {
-        #Write down to local disk to maintain a temp. seperate reference
-        $uResponse.Content | Out-File "C:\temp\azure-usage-$enrollmentNo.json"
+    $usageDetails = @()
+    while ($usageUrl -ne $null) #Looping as there are only 1000 lines of usage data returned per request
+	{
+		$uResponse = Invoke-WebRequest $usageUrl -Headers $authHeaders -ErrorAction Stop
+		if ($uResponse.StatusCode -eq 200) {
+			# Keep a copy of the content data portion (warning: this may get quite large)
+			$usageDetails += ($uResponse.Content | ConvertFrom-Json).Data
 
-        #Convert into proper PSObject
-        $usageDetails = $uResponse.Content | ConvertFrom-Json
+			# Extract next pagination link
+			$usageUrl = ($uResponse.Content | ConvertFrom-Json).nextLink
+		}
+	}
 
-        return $usageDetails
-    }
+    return $usageDetails
 }
 
 
@@ -53,5 +57,6 @@ $accesskey = ""
 $pricesheet = Get-AzureBillPricesheet $enrollmentNo $accesskey
 $usage = Get-AzureBillUsage $enrollmentNo $accesskey
 
-
-
+$usage.count
+$usage | ConvertTo-Json | Out-File "C:\temp\azure-usage-$enrollmentNo.json"
+$usage | Export-Csv "C:\temp\azure-usage-$enrollmentNo.csv"
