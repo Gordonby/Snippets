3a1e3c85e21a35a29cd09bbfeea97f1e78175993
Gordon Byers <gordonbyers@gmail.com>
2017-08-07 16:37:22 +0100

    Takes Azure Billing data in OMS
    Json output from the V2 Billing API, into OMS.

---
+++ Post-AzureBillingDataToOMS.ps1
@@ -0,0 +1,66 @@
+#Gordon.byers@microsoft.com
+#Powershell script is provided as-is and without any warranty of any kind
+
+Function Build-Signature ($customerId, $sharedKey, $date, $contentLength, $method, $contentType, $resource)
+{
+    $xHeaders = "x-ms-date:" + $date
+    $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
+
+    $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
+    $keyBytes = [Convert]::FromBase64String($sharedKey)
+
+    $sha256 = New-Object System.Security.Cryptography.HMACSHA256
+    $sha256.Key = $keyBytes
+    $calculatedHash = $sha256.ComputeHash($bytesToHash)
+    $encodedHash = [Convert]::ToBase64String($calculatedHash)
+    $authorization = 'SharedKey {0}:{1}' -f $customerId,$encodedHash
+    return $authorization
+}
+
+Function Post-OMSData($customerId, $sharedKey, $body, $logType)
+{
+    $method = "POST"
+    $contentType = "application/json"
+    $resource = "/api/logs"
+    $rfc1123date = [DateTime]::UtcNow.ToString("r")
+    $contentLength = $body.Length
+    $signature = Build-Signature `
+        -customerId $customerId `
+        -sharedKey $sharedKey `
+        -date $rfc1123date `
+        -contentLength $contentLength `
+        -fileName $fileName `
+        -method $method `
+        -contentType $contentType `
+        -resource $resource
+    $uri = "https://" + $customerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
+
+    $headers = @{
+        "Authorization" = $signature;
+        "Log-Type" = $logType;
+        "x-ms-date" = $rfc1123date;
+        "time-generated-field" = $TimeStampField;
+    }
+
+    $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
+    return $response.StatusCode
+
+}
+
+#Main Params
+[string] $CustomerId = ""
+[string] $SharedKey = ""
+[string] $LogType = "AzureBill"
+[string] $TimeStampField = "date"
+[string] $jsonFile = "C:\Temp\azure-usage-12345678_2017-08-01_(0).json"   
+  
+#Json content
+$json= Get-Content -Raw -Path $jsonFile
+$tooBig = $json.Length -gt 30mb
+
+if ($tooBig -ne $true) {
+    Post-OMSData -customerId $customerId -sharedKey $sharedKey -body ([System.Text.Encoding]::UTF8.GetBytes($json)) -logType $logType  
+}
+else {
+    Write-Error "Json from $jsonFile too big!  Exceeds 30MB data ingress limit"
+}
