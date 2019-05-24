d60d8c013c2ad7d2337156c579bfe35d2485a94e
Gordon Byers <gordonbyers@gmail.com>
2019-05-24 21:02:39 +0100

    Create ResourceGraphQueryVm.ps1

---
+++ Powershell/ResourceGraphQueryVm.ps1
@@ -0,0 +1,16 @@
+﻿# Install the Resource Graph module from PowerShell Gallery
+Install-Module -Name Az.ResourceGraph
+
+# Connect to Azure
+Connect-AzAccount
+
+# Filter to the right subscriptions
+$subscriptions = Get-AzSubscription | ? {$_.name -like "Contoso*"} | select  Id -ExpandProperty
+
+# Compose query
+$query="where type == 'microsoft.compute/virtualmachines' |
+where tags.environment == 'dev' |
+project subscriptionId, resourceGroup, name, properties.hardwareProfile.vmSize, properties.licenseType"
+
+# Run query
+Search-AzGraph -Query $query -Subscription $subscriptions
