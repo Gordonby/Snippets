d64a74516f3c19e8117e991988692b56ded3c118
Gordon Byers <gordonbyers@gmail.com>
2019-06-12 10:34:05 +0100

    Where tag doesnt exist

---
+++ Powershell/ResourceGraphQueryVmNoTags.ps1
@@ -0,0 +1,18 @@
+﻿# Install the Resource Graph module from PowerShell Gallery
+Install-Module -Name Az.ResourceGraph
+
+# Connect to Azure
+Connect-AzAccount
+
+# Filter to the right subscriptions
+$subscriptions = Get-AzSubscription | ? {$_.name -like "Contoso*"} | select -ExpandProperty Id 
+
+# Compose query
+$query="where type == 'microsoft.compute/virtualmachines' |
+where tags !hasprefix 'orchestrator' |
+project subscriptionId, resourceGroup, name, properties.hardwareProfile.vmSize, properties.licenseType, tags"
+
+# Run query
+$results = Search-AzGraph -Query $query -Subscription $subscriptions
+
+$results
