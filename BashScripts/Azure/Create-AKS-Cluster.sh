ff4effe7970c0894b4fd0987dbb69dd704f875a4
Gordon Byers <gordon.byers@microsoft.com>
2019-10-09 19:58:24 +0100

    Update Create-AKS-Cluster.sh

--- BashScripts/Azure/Create-AKS-Cluster.sh
+++ BashScripts/Azure/Create-AKS-Cluster.sh
@@ -1,4 +1,29 @@
-az group create --name $1 --location eastus --tag Environment=Prod
-az role assignment create --role Contributor --resource-group $1 --assignee 9cebaf93-813d-489e-b027-6532a125012c
-az aks create --resource-group $1 --name $1Cluster --node-count 1 --generate-ssh-keys --kubernetes-version 1.9.6 --node-vm-size=Standard_B2s
-az aks get-credentials --resource-group $1 --name $1Cluster
+LOC="westeurope"
+
+PREFIX="g"
+SUFFIX=$(date -d "$D" '+%d%m')#
+
+MONSUBID=""
+MONRG=""
+MONNAME=""
+
+VERSION=$(az aks get-versions -l $LOC --query 'orchestrators[-1].orchestratorVersion' -o tsv)
+
+az aks create \
+    --resource-group K8s \ 
+    --location $LOC \
+    --name ${PREFIX}aks${SUFFIX} \
+    --vm-set-type VirtualMachineScaleSets \
+    --node-count 1 \
+    --enable-vmss \
+    --enable-cluster-autoscaler \
+    --min-count 1 \
+    --max-count 3 \
+    --attach-acr gordopremiumreg \
+    --load-balancer-sku standard \
+    --generate-ssh-keys \
+    --node-vm-size=Standard_B2s \
+    --kubernetes-version $VERSION \
+    --generate-ssh-keys \
+    --enable-addons monitoring \
+    --workspace-resource-id "/subscriptions/${MONSUBID}/resourcegroups/${MONRG}/providers/microsoft.operationalinsights/workspaces/${MONNAME}"
