791c5504921454f0eecc3e86794947c44882985a
Gordon Byers <gordon.byers@microsoft.com>
2020-02-03 16:14:25 -0800

    Update Create-AKS-Cluster.sh

--- BashScripts/Azure/Create-AKS-Cluster.sh
+++ BashScripts/Azure/Create-AKS-Cluster.sh
@@ -1,38 +1,64 @@
+echo "Checking for updates for the AKS preview extension"
+az extension update -n aks-preview
+
+echo "Displaying current subscription context"
+az account show -o table
+
 LOC="eastus"
 
-PREFIX=""
+PREFIX="g5"
 SUFFIX=$(date -d "$D" '+%d%m')
 
 USERNAME_WIN=""
 PASSWORD_WIN="P@ssw0rd1234"
 
-MONSUBID=""
-MONRG=""
-MONNAME=""
+MONRG="oms"
+MONNAME="gobyers"
 
 ACRNAME="gordopremiumreg"
 
 VERSION=$(az aks get-versions -l $LOC --query 'orchestrators[-1].orchestratorVersion' -o tsv)
 
+echo "Checking Variables..."
+echo "Suffix is $SUFFIX"
+echo "Version is $VERSION"
+
+echo "Checking Azure dependant resources exist..."
+echo "  Checking Azure Container Registry..."
+az acr show -n $ACRNAME -o table
+
+echo "  Checking Monitoring Insights exists..."
+az resource show -g $MONRG -n $MONNAME --resource-type Microsoft.OperationalInsights/workspaces -o table
+
+echo "  Monitoring Insights Resouce Id"
+MONID=$(az resource show -g $MONRG -n $MONNAME --resource-type Microsoft.OperationalInsights/workspaces -o json --query 'id' -o tsv)
+echo $MONID
+
+read -p "Still want to create the cluster? [use ctrl+c to cancel] " ANSWER
+
+echo "Creating the cluster"
 az aks create -n ${PREFIX}aks${SUFFIX} \
     -g K8s --location $LOC \
     --vm-set-type VirtualMachineScaleSets \
     --node-count 1 \
     --enable-vmss \
     --enable-cluster-autoscaler \
-    --min-count 1 --max-count 2 \
-    --attach-acr $ACRNAME \
+    --min-count 1 --max-count 4 \
     --load-balancer-sku standard \
     --generate-ssh-keys \
     --node-vm-size=Standard_B2s \
     --kubernetes-version $VERSION \
-    --generate-ssh-keys \
     --windows-admin-password $PASSWORD_WIN \
     --windows-admin-username $USERNAME_WIN \
     --network-plugin azure \
     --enable-addons monitoring \
-    --workspace-resource-id "/subscriptions/${MONSUBID}/resourcegroups/${MONRG}/providers/microsoft.operationalinsights/workspaces/${MONNAME}"
+    --workspace-resource-id $MONID \
+    --verbose
 
+echo "Adding the ACR role"
+  az aks update -n ${PREFIX}aks${SUFFIX} -g K8s --attach-acr $ACRNAME
+
+echo "Adding windows nodepool"
   az aks nodepool add \
     --resource-group K8s \
     --cluster-name ${PREFIX}aks${SUFFIX} \
@@ -43,4 +69,9 @@ az aks create -n ${PREFIX}aks${SUFFIX} \
     --node-vm-size=Standard_B2s \
     --enable-cluster-autoscaler \
     --min-count 1 \
-    --max-count 2
+    --max-count 2 \
+    --node-taints key=value:NoSchedule
+
+
+echo "Adding clusterrolebinding for dashboard"
+kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
