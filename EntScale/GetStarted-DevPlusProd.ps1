0fa8c0641440198208f55b293d56415b326373d7
Gordon Byers <gobyers@microsoft.com>
2021-03-24 09:42:39 +0000

    Typos, and clearer comments

--- EntScale/GetStarted-DevPlusProd.ps1
+++ EntScale/GetStarted-DevPlusProd.ps1
@@ -18,18 +18,17 @@ $prodMgScope = "/providers/Microsoft.Management/managementGroups/$prodBootstrapP
 
 #Create Service Principal and assign Owner role to the right scopes
 $servicePrincipals=@()
-$servicePrincipals += New-AzADServicePrincipal -DisplayName AzOpsRootReader
+$servicePrincipals += New-AzADServicePrincipal -DisplayName AzOpsReader
 #$servicePrincipals += New-AzADServicePrincipal -DisplayName AzOpsCredRotateHelper
 $servicePrincipals += New-AzADServicePrincipal -Role Owner -Scope $devMgScope -DisplayName AzOpsDev
 $servicePrincipals += New-AzADServicePrincipal -Role Owner -Scope $prodMgScope -DisplayName AzOpsProd
-
 
 New-AzRoleAssignment -Scope $devMgScope -RoleDefinitionName 'Reader' -ObjectId $servicePrincipals[0].Id
 New-AzRoleAssignment -Scope $prodMgScope -RoleDefinitionName 'Reader' -ObjectId $servicePrincipals[0].Id
 
 $servicePrincipals | % {
     #Provide reader access to the current subscription.
-    #AzOps requires Service Principals have at least some RBAC on a default subscription
+    #AzOps requires Service Principals have at least some RBAC on a default subscription, the current subscription context is used for simplicity.
     New-AzRoleAssignment -ObjectId $_.Id -RoleDefinitionName Reader -Scope "/subscriptions/$((Get-AzContext).Subscription.Id)"
 
     #Prettify output to print in the format for AZURE_CREDENTIALS
@@ -62,10 +61,11 @@ $AzureAdCred = Get-Credential
 Connect-AzureAD -Credential $AzureAdCred
 
 #Get AzOps Service Principal from Azure AD
-$aadServicePrincipals = Get-AzureADServicePrincipal -Filter "DisplayName eq 'AzOpsRootReader'"
+$aadServicePrincipal = Get-AzureADServicePrincipal -Filter "DisplayName eq 'AzOpsReader'"
 
 #Get Azure AD Directory Role
 $DirectoryRole = Get-AzureADDirectoryRole -Filter "DisplayName eq 'Directory Readers'"
+#$DirectoryRole = Get-AzureADDirectoryRole | Where-Object {$_.DisplayName -eq "Directory Readers"} #If the line above doesn't work with the Filter param, then do this.
 
 if ($DirectoryRole -eq $NULL) {
     Write-Output "Directory Reader role not found. This usually occurs when the role has not yet been used in your directory"
--- EntScale/Reduce-Scope-Assignment-For-ServicePrincipal.ps1
+++ EntScale/Reduce-Scope-Assignment-For-ServicePrincipal.ps1
@@ -1,15 +1,16 @@
-﻿#Define management group variables
+﻿#Script is designed to reduce the RoleAssignments afforded to the AzOps Service Principal, in the event Root Management Group scope access was set.
+
+#Define management group top levels where the Service Principal should have Read access
 $devBootstrapPrefix="codev"
 $prodBootstrapPrefix="coprd"
 
 
-
-#Define management group ids
+#Define management group Resource Ids
 $devMgScope = "/providers/Microsoft.Management/managementGroups/$devBootstrapPrefix"
 $prodMgScope = "/providers/Microsoft.Management/managementGroups/$prodBootstrapPrefix"
 
 #Retrieve the Service Principal
-$sp = Get-AzADServicePrincipal -DisplayName "AzOpsRootReader"
+$sp = Get-AzADServicePrincipal -DisplayName "AzOpsReader"
 
 #Remove any Root scope assignments that have been made for this Service Principal
 Get-AzRoleAssignment -Scope "/" -ObjectId $sp.Id | % {
