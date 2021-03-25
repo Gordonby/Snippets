cb74a3e583f87e2c895e1aadea9c5965b2209e99
Gordon Byers <gobyers@microsoft.com>
2021-03-25 12:12:57 +0000

    Added ADO variable updating

--- EntScale/GetStarted-DevPlusProd.ps1
+++ EntScale/GetStarted-DevPlusProd.ps1
@@ -17,33 +17,61 @@ $devMgScope = "/providers/Microsoft.Management/managementGroups/$devBootstrapPre
 $prodMgScope = "/providers/Microsoft.Management/managementGroups/$prodBootstrapPrefix"
 
 #Create Service Principal and assign Owner role to the right scopes
-$servicePrincipals=@()
-$servicePrincipals += New-AzADServicePrincipal -DisplayName AzOpsReader
-#$servicePrincipals += New-AzADServicePrincipal -DisplayName AzOpsCredRotateHelper
-$servicePrincipals += New-AzADServicePrincipal -Role Owner -Scope $devMgScope -DisplayName AzOpsDev
-$servicePrincipals += New-AzADServicePrincipal -Role Owner -Scope $prodMgScope -DisplayName AzOpsProd
+$readerSP = New-AzADServicePrincipal -DisplayName AzOpsReader6
+New-AzRoleAssignment -Scope $devMgScope -RoleDefinitionName 'Reader' -ObjectId $readerSP.Id
+New-AzRoleAssignment -Scope $prodMgScope -RoleDefinitionName 'Reader' -ObjectId $readerSP.Id
 
-New-AzRoleAssignment -Scope $devMgScope -RoleDefinitionName 'Reader' -ObjectId $servicePrincipals[0].Id
-New-AzRoleAssignment -Scope $prodMgScope -RoleDefinitionName 'Reader' -ObjectId $servicePrincipals[0].Id
+#$rotaterSP = New-AzADServicePrincipal -DisplayName AzOpsCredRotateHelper
+$devSP = New-AzADServicePrincipal -Role Owner -Scope $devMgScope -DisplayName AzOpsDev6
+$prodSP = New-AzADServicePrincipal -Role Owner -Scope $prodMgScope -DisplayName AzOpsProd6
 
-$servicePrincipals | % {
-    #Provide reader access to the current subscription.
-    #AzOps requires Service Principals have at least some RBAC on a default subscription, the current subscription context is used for simplicity.
-    New-AzRoleAssignment -ObjectId $_.Id -RoleDefinitionName Reader -Scope "/subscriptions/$((Get-AzContext).Subscription.Id)"
+#Provide reader access to the current subscription.
+#AzOps requires Service Principals have at least some RBAC on a default subscription, the current subscription context is used for simplicity.
+New-AzRoleAssignment -ObjectId $readerSP.Id -RoleDefinitionName Reader -Scope "/subscriptions/$((Get-AzContext).Subscription.Id)"
+New-AzRoleAssignment -ObjectId $devSP.Id -RoleDefinitionName Reader -Scope "/subscriptions/$((Get-AzContext).Subscription.Id)"
+New-AzRoleAssignment -ObjectId $prodSP.Id -RoleDefinitionName Reader -Scope "/subscriptions/$((Get-AzContext).Subscription.Id)"
 
+###############################################################
+# Specify function to easily get Json from a ServicePrincipal #
+###############################################################
+function GetJsonFromSP($SP, $removeCrlf=$true) {
     #Prettify output to print in the format for AZURE_CREDENTIALS
     $spDevJson=[ordered]@{
-        clientId = $_.ApplicationId
-        displayName = $_.DisplayName
-        name = $_.ServicePrincipalNames[1]
-        clientSecret = [System.Net.NetworkCredential]::new("", $_.Secret).Password
+        clientId = $SP.ApplicationId
+        displayName = $SP.DisplayName
+        name = $SP.ServicePrincipalNames[1]
+        clientSecret = [System.Net.NetworkCredential]::new("", $SP.Secret).Password
         tenantId = (Get-AzContext).Tenant.Id
         subscriptionId = (Get-AzContext).Subscription.Id
     } | ConvertTo-Json
     $spDevJsonE = $spDevJson.Replace('"','\"')
-    Write-Output $spDevJsonE
+    
+    if($removeCrlf -eq $true) {$spDevJsonE= $spDevJsonE -replace "`t|`n|`r",""}
+    return $spDevJsonE
 }
 
+######################################
+# Optionally, Output SPN's to screen #
+######################################
+Write-Output "$(GetJsonFromSP $readerSP $false)"
+Write-Output "$(GetJsonFromSP $prodSP $false)"
+Write-Output "$(GetJsonFromSP $devSP $false)" 
+
+###################################################
+# Update existing Azure DevOps Pipeline Variables #
+###################################################
+$org="https://gdoggmsft.visualstudio.com/"
+$project="EntScaleT4"
+
+az extension add -n "azure-devops"
+az login --use-device-code #az devops login --organization "$org" 
+az devops configure --defaults organization=$org project=$project
+
+az pipelines variable update --name AZURE_CREDENTIALS --pipeline-name AzOps-Pull --secret true --value "$(GetJsonFromSP($readerSP))"
+az pipelines variable update --name AZURE_CREDENTIALS --pipeline-name AzOps-Prod-Push --secret true --value "$(GetJsonFromSP($prodSP))"
+az pipelines variable update --name AZURE_CREDENTIALS --pipeline-name AzOps-Dev-Push --secret true --value "$(GetJsonFromSP($devSP))"
+
+#az pipelines variable-group create --name "AZURE_CREDENTIALS" --variables "$devBootstrapPrefix=$(GetJsonFromSP($devSP)) $prodBootstrapPrefix=$(GetJsonFromSP($prodSP))"
 
 ########################################
 # Configure the AzureAD directory role #
@@ -75,3 +103,6 @@ else {
     #Add service principal to Directory Role
     Add-AzureADDirectoryRoleMember -ObjectId $DirectoryRole.ObjectId -RefObjectId $aadServicePrincipal.ObjectId
 }
+
+
+
