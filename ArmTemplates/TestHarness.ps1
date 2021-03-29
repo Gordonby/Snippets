4e83f2a52db5bafaf26a328702166cd55eaa596d
Gordon Byers <gobyers@microsoft.com>
2021-03-29 10:45:58 +0100

    A few sample Subscription level templates

---
+++ ArmTemplates/TestHarness.ps1
@@ -0,0 +1,19 @@
+﻿
+#$path="C:\Users\gobyers\OneDrive - Microsoft\CustomerSpecifics\Barclays\DCT-SubVend-FORK\Azure-Barclays-Platform\armlib\resourcegroups\EmptyResourceGroup.json"
+$path="C:\ReposGitHub\Snippets\ArmTemplates\ResourceGroupWithStorage.json"
+
+$templateParams = @{
+    'rgName' = "gordarmtest4"
+    'principalId' = "d39ba6a7-bd94-42bd-806b-dbce9e4dc198"
+    'roleDefinition' = 'NetworkContributor'
+}
+New-AzSubscriptionDeployment -Location WestEurope -TemplateFile $path -TemplateParameterObject $templateParams -SkipTemplateParameterPrompt
+
+$path = "C:\ReposGitHub\Snippets\ArmTemplates\ResourceGroupWithStorageAndFunctionHosting.json"
+$templateParams = @{
+    'rgName' = "gordarmtest8"
+    'FunctionAppName' = "jammyjamjar"
+}
+
+New-AzSubscriptionDeployment -Location WestEurope -TemplateFile $path -TemplateParameterObject $templateParams -SkipTemplateParameterPrompt
+
