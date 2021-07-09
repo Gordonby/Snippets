77543dda0aaefbb82c862d3e35122480c852f01c
Gordon Byers <gordonbyers@gmail.com>
2021-07-09 07:25:47 +0100

    Update commands.sh

--- AKS/commands.sh
+++ AKS/commands.sh
@@ -1,4 +1,8 @@
+#lookup images in the mcr (good for when behind az fw)
+wget -O - https://mcr.microsoft.com/v2/_catalog | grep busybox
+wget -O - https://mcr.microsoft.com/v2/aks/e2e/library-busybox/tags/list
+
+
 #busybox for debugging.  ping/dns/wget-magic :D
-#using mcr hosted for when behind az fw.
 kubectl run -i --tty --rm debug --image=mcr.microsoft.com/aks/e2e/library-busybox:master.210526.1 --restart=Never -- sh
 
