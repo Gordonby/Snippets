b28b4b895232ecce1376dd227ed6aa75237b5330
Gordon Byers <gordon.byers@microsoft.com>
2021-12-03 15:45:08 +0000

    Update LogAnalyticsInfo.ps1

--- Powershell/AzureDeploymentScripts/LogAnalyticsInfo.ps1
+++ Powershell/AzureDeploymentScripts/LogAnalyticsInfo.ps1
@@ -1,5 +1,3 @@
-$ErrorActionPreference = 'Stop'
-
 param([string] $logAnalyticsName, [string] $resourceGroupName)
 Write-Output "Searching for $logAnalyticsName in $resourceGroupName"
 $resource=Get-AzResource -Name $logAnalyticsName -ResourceGroupName $resourceGroupName
