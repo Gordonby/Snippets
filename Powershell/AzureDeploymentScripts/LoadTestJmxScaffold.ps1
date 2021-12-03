22e1f708fa82edc43118c51f02b620aadf8a8ef1
Gordon Byers <gordon.byers@microsoft.com>
2021-12-03 16:54:14 +0000

    Create LoadTestJmxScaffold.ps1

---
+++ Powershell/AzureDeploymentScripts/LoadTestJmxScaffold.ps1
@@ -0,0 +1,13 @@
+param([string] $LoadTestResourceName, [string] $ResourceGroup, [string] $TestName, [string] $TestUrl, [string] $TestMethod="Get")
+$debug = 'Hello {0}' -f $TestUrl
+Write-Output $debug
+Write-Output "Getting Base Jmeter XML"
+Write-Output "Injecting URL into XML"
+Write-Output "Persisting Jmeter XML file to local runspace"
+Write-Output "Adding new Test $TestName to $LoadTestResourceName"
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['debug'] = $debug
+$DeploymentScriptOutputs['loadtest'] = $LoadTestResourceName
+$DeploymentScriptOutputs['rg'] = $ResourceGroup
+$DeploymentScriptOutputs['method'] = $TestMethod
+$DeploymentScriptOutputs['testurl'] = $TestUrl
