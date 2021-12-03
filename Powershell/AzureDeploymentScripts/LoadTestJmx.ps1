e4755bb2ef599f77e3537908dee8285ec8b216c2
Gordon Byers <gordon.byers@microsoft.com>
2021-12-03 16:34:05 +0000

    Update LoadTestJmx.ps1

--- Powershell/AzureDeploymentScripts/LoadTestJmx.ps1
+++ Powershell/AzureDeploymentScripts/LoadTestJmx.ps1
@@ -1,7 +1,10 @@
 param([string] $loadtestname, [string] $resourcegroup, [string] $url)
 $output = 'Hello {0}' -f $url
 Write-Output $output
-
+Write-Output "Getting Base Jmeter XML"
+Write-Output "Injecting URL into XML"
+Write-Output "Persisting Jmeter XML file to local runspace"
+Write-Output "Adding new Test"
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['text'] = $output
 $DeploymentScriptOutputs['loadtest'] = $loadtestname
