4b0df0995230a0bfa7b130ab260be8890d9f9a7c
Gordon Byers <gordon.byers@microsoft.com>
2021-12-03 15:51:53 +0000

    Update LogAnalyticsInfo.ps1

--- Powershell/AzureDeploymentScripts/LogAnalyticsInfo.ps1
+++ Powershell/AzureDeploymentScripts/LogAnalyticsInfo.ps1
@@ -1,5 +1,2 @@
 param([string] $logAnalyticsName, [string] $resourceGroupName)
 Write-Output "Searching for $logAnalyticsName in $resourceGroupName"
-$resource=Get-AzResource -Name $logAnalyticsName -ResourceGroupName $resourceGroupName
-$DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['text'] = $resource.ResourceId
