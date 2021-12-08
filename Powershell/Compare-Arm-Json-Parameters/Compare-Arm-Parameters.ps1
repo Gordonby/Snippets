f32ed0209cae44142f30ad1ff75f10e8095e3d13
Gordonby <gordon.byers@microsoft.com>
2021-12-08 12:40:59 +0000

    removed hash table

--- Powershell/Compare-Arm-Json-Parameters/Compare-Arm-Parameters.ps1
+++ Powershell/Compare-Arm-Json-Parameters/Compare-Arm-Parameters.ps1
@@ -4,45 +4,26 @@ $arm2filepath="main2.json"
 $arm1params = get-content $arm1filepath | ConvertFrom-Json -AsHashtable | Select -expandProperty parameters
 $arm2params = get-content $arm2filepath | ConvertFrom-Json -AsHashtable | Select -expandProperty parameters
 
-# $arm1hash = @{}
-# $arm1paramList = @()
-# $arm1params.keys | % { Write-Output $_; $param = New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$arm1params.Get_Item($_).defaultValue }); $arm1paramList+= $param; $arm1hash.add($_, $arm1params.Get_Item($_).defaultValue) }
-
-#powershell function to get the default value of a parameter
-
-$arm1hash = @{}
 $arm1paramList = @()
 $arm1params.keys | % {
-    Write-Output "--Processing $_";
     $val=$arm1params.Get_Item($_).defaultValue
-    Write-Output "--Val $val";
-    Write-Output $arm1params.Get_Item($_).type
     if( "int","string" -contains $arm1params.Get_Item($_).type) {
         $val=$arm1params.Get_Item($_).defaultValue
     } else {
         $val=$arm1params.Get_Item($_).defaultValue | ConvertTo-Json -Compress
     }
-    $param = New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$val });
-    Write-Output $param
-    $arm1paramList+= $param; $arm1hash.add($_, $arm1params.Get_Item($_).defaultValue)
+    $arm1paramList += New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$val });
 }
 
-$arm2hash = @{}
 $arm2paramList = @()
 $arm2params.keys | % {
-    Write-Output "--Processing $_";
     $val=$arm2params.Get_Item($_).defaultValue
-    Write-Output "--Val $val";
-    Write-Output $arm2params.Get_Item($_).type
     if( "int","string" -contains $arm2params.Get_Item($_).type) {
         $val=$arm2params.Get_Item($_).defaultValue
     } else {
         $val=$arm2params.Get_Item($_).defaultValue | ConvertTo-Json -Compress
     }
-    $param = New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$val });
-    Write-Output $param
-    $arm2paramList+= $param; $arm2hash.add($_, $arm2params.Get_Item($_).defaultValue)
+    $arm2paramList+= New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$val });
 }
 
-
-Compare-Object $arm1paramList $arm2paramList -Property Name, DefaultValue
+Compare-Object $arm1paramList $arm2paramList -Property Name, DefaultValue -PassThru | Sort-Object Name, SideIndicator | Format-Table -AutoSize
