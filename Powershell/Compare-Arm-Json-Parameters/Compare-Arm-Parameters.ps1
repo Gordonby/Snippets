d2f06eceab26a781cdfadf57555dc0c97bf7d692
Gordon Byers <gordon.byers@microsoft.com>
2021-12-08 13:57:25 +0000

    add whitespace

--- Powershell/Compare-Arm-Json-Parameters/Compare-Arm-Parameters.ps1
+++ Powershell/Compare-Arm-Json-Parameters/Compare-Arm-Parameters.ps1
@@ -11,3 +11,5 @@ $arm2paramList = @()
 $arm2params.keys | % {$arm2paramList+= New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$arm2params.Get_Item($_).defaultValue | ConvertTo-Json -Compress })}
 
 Compare-Object $arm1paramList $arm2paramList -Property Name, DefaultValue -PassThru | Sort-Object Name, SideIndicator | Format-Table -AutoSize
+
+
