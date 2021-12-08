f2be629bc18548383ca44f3709ec9b9965da5965
Gordonby <gordon.byers@microsoft.com>
2021-12-08 12:49:33 +0000

    Final version of the PowerShell

--- Powershell/Compare-Arm-Json-Parameters/Compare-Arm-Parameters.ps1
+++ Powershell/Compare-Arm-Json-Parameters/Compare-Arm-Parameters.ps1
@@ -5,13 +5,9 @@ $arm1params = get-content $arm1filepath | ConvertFrom-Json -AsHashtable | Select
 $arm2params = get-content $arm2filepath | ConvertFrom-Json -AsHashtable | Select -expandProperty parameters
 
 $arm1paramList = @()
-$arm1params.keys | % {
-    $arm1paramList += New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$arm1params.Get_Item($_).defaultValue | ConvertTo-Json -Compress });
-}
+$arm1params.keys | % {$arm1paramList += New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$arm1params.Get_Item($_).defaultValue | ConvertTo-Json -Compress })}
 
 $arm2paramList = @()
-$arm2params.keys | % {
-    $arm2paramList+= New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$arm2params.Get_Item($_).defaultValue | ConvertTo-Json -Compress });
-}
+$arm2params.keys | % {$arm2paramList+= New-Object PSObject -Property ([Ordered]@{Name=$_; DefaultValue=$arm2params.Get_Item($_).defaultValue | ConvertTo-Json -Compress })}
 
 Compare-Object $arm1paramList $arm2paramList -Property Name, DefaultValue -PassThru | Sort-Object Name, SideIndicator | Format-Table -AutoSize
