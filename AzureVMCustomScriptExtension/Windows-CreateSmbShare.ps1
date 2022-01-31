9753c0626ec63a8965266ffdb8b5e807dfd9a890
Gordon Byers <gordon.byers@microsoft.com>
2022-01-31 11:08:30 +0000

    Update Windows-CreateSmbShare.ps1

--- AzureVMCustomScriptExtension/Windows-CreateSmbShare.ps1
+++ AzureVMCustomScriptExtension/Windows-CreateSmbShare.ps1
@@ -4,3 +4,6 @@ $dirPath = "c:\$dirName"
 New-Item $dirPath -type directory
 
 New-SMBShare -Name $dirName -Path $dirPath
+
+$Password = "zeP4ssW0RD%%" | ConvertTo-SecureString -AsPlainText -Force
+PS C:\Users\admingeneric> New-LocalUser "user1" -Password $Password -FullName "aks smb test" -Description "Account to demo smb share"
