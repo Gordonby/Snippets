7ace081fd2936306d5486d56a4a37ea1cc5436cf
Gordon Byers <gordon.byers@microsoft.com>
2022-01-31 12:12:10 +0000

    Update Smb-Demo-Pt1.sh

--- AKS/Smb-Demo-Pt1.sh
+++ AKS/Smb-Demo-Pt1.sh
@@ -1,6 +1,7 @@
 #Specify SMB share info
 smbServerAddress="172.21.1.37"
 shareName="SmbShare"
+anonShareName="anon"
 
 #Install driver to existing cluster
 helm repo add csi-driver-smb https://raw.githubusercontent.com/kubernetes-csi/csi-driver-smb/master/charts
@@ -28,6 +29,57 @@ spec:
     volumeHandle: gord-smb-demo1
     volumeAttributes:
       source: "//$smbServerAddress/$shareName"
+    nodeStageSecretRef:
+      name: smbcreds
+      namespace: default
+" | kubectl apply -f -
+
+share
+echo "
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv-smb-gord-demo-anon-no-creds
+spec:
+  capacity:
+    storage: 100Gi
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Retain
+  mountOptions:
+    - dir_mode=0777
+    - file_mode=0777
+    - vers=3.0
+  csi:
+    driver: smb.csi.k8s.io
+    readOnly: false
+    volumeHandle: gord-smb-demo-anon1
+    volumeAttributes:
+      source: "//$smbServerAddress/$anonShareName"
+" | kubectl apply -f -
+
+share
+echo "
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: pv-smb-gord-demo-anon-arb-creds
+spec:
+  capacity:
+    storage: 100Gi
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Retain
+  mountOptions:
+    - dir_mode=0777
+    - file_mode=0777
+    - vers=3.0
+  csi:
+    driver: smb.csi.k8s.io
+    readOnly: false
+    volumeHandle: gord-smb-demo-anon2
+    volumeAttributes:
+      source: "//$smbServerAddress/$anonShareName"
     nodeStageSecretRef:
       name: smbcreds
       namespace: default
