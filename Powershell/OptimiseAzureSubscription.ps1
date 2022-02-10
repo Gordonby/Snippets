6f647f90294478e76d2da3822f9e9102d446690e
Gordon Byers <gordon.byers@microsoft.com>
2022-02-10 12:47:24 +0000

    Update OptimiseAzureSubscription.ps1

--- Powershell/OptimiseAzureSubscription.ps1
+++ Powershell/OptimiseAzureSubscription.ps1
@@ -9,7 +9,7 @@ $webapps=Get-AzWebApp
 $webapps | ? {$_.HttpsOnly -ne $true} | % {Write-Output "Updating web app $($_.Name) to HttpsOnly"; $_ | Set-AzWebApp -ResourceGroupName $_.ResourceGroup -HttpsOnly $true }
 
 Write-Output "Bad NSG rules, should be using JIT"
-$badPorts="22","3389","*"
+$badPorts="22","3389","*", "445", "5985", "5986"
 $nsgs=Get-AzNetworkSecurityGroup
 $nsgSecurityRulesToRemove = $nsgs | % {$nsgName=$_.Name; $nsg=$_ ;$_.SecurityRules | ? {$_.Direction -eq "Inbound" -and $_.Access -eq "Allow" -and $_.Description -cnotlike "CSS Governance Security Rule*" -and $badPorts -contains $_.DestinationAddressPrefix} | select Name,DestinationAddressPrefix, @{Name="NetworkSecurityGroup"; Expression={$nsg}}}
 $nsgSecurityRulesToRemove | %{Write-Output "Removing *bad nsg* rule $($_.Name) from $($_.NetworkSecurityGroup.Name)"; Remove-AzNetworkSecurityRuleConfig -Name $_.Name -NetworkSecurityGroup $_.NetworkSecurityGroup}
