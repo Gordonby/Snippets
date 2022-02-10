19e950d68ec66535aeaec4991ffb3be433e248c4
Gordon Byers <gordon.byers@microsoft.com>
2022-02-10 09:17:13 +0000

    Update OptimiseAzureSubscription.ps1

--- Powershell/OptimiseAzureSubscription.ps1
+++ Powershell/OptimiseAzureSubscription.ps1
@@ -1,5 +1,6 @@
-$storageAccounts=Get-AzStorageAccount
 
+Write-Output "Storage Accounts"
+$storageAccounts=Get-AzStorageAccount
 $storageAccounts | ?{$_.Kind -eq "Storage"} | % {Write-Output "Updating storage account $($_.StorageAccountName) to v2"; $_ | Set-AzStorageAccount -UpgradeToStorageV2}
 $storageAccounts | ?{$_.MinimumTlsVersion -ne "TLS1_2"} | % {Write-Output "Updating storage account $($_.StorageAccountName) to TLS1.2"; $_ | Set-AzStorageAccount -MinimumTlsVersion TLS1_2}
 
