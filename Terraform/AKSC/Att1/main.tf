fe0a9a49d6eb589bb137e02df193a588f5fce5ae
Gordon Byers <gordon.byers@microsoft.com>
2022-04-01 22:03:15 +0100

    Update main.tf

--- Terraform/AKSC/Att1/main.tf
+++ Terraform/AKSC/Att1/main.tf
@@ -1,14 +1,16 @@
+#main.tf
 data "http" "aksc_release" {
   url = "https://github.com/Azure/AKS-Construction/releases/download/0.6.2/main.json"
   request_headers = {
     Accept = "application/json"
+    User-Agent = "request module"
   }
 }
 
 data "azurerm_client_config" "azcontext" {}
 
 resource "azurerm_resource_group" "rg" {
-  name = "az-k8s-eiwl-rg"
+  name = "az-k8s-jvc6-rg"
   location = "WestEurope"
 }
 
@@ -18,9 +20,12 @@ resource "azurerm_resource_group_template_deployment" "aksc_deploy" {
   deployment_mode = "Incremental"
   template_content = data.http.aksc_release.body
   parameters_content = jsonencode({
-    location = azurerm_resource_group.rg.location
-    resourceName = var.resourceName
-    JustUseSystemPool = var.JustUseSystemPool
-    agentCountMax = var.agentCountMax
+    resourceName = {value=var.resourceName}
+    JustUseSystemPool = {value=var.JustUseSystemPool}
+    agentCountMax = {value=var.agentCountMax}
+    registries_sku = {value=var.registries_sku}
+    #acrPushRolePrincipalId = {value=var.acrPushRolePrincipalId}
+    omsagent = {value=var.omsagent}
+    retentionInDays = {value=var.retentionInDays}
   })
 }
