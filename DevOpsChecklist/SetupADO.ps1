b312a336318df6a14b0a0ae7502c3d5330be7550
Gordon Byers <gordon.byers@microsoft.com>
2023-01-10 10:16:18 +0000

    Delete SetupADO.ps1

--- DevOpsChecklist/SetupADO.ps1
+++
@@ -1,9 +0,0 @@
-﻿$org="Barclays-DEV-LandingZone"
-$proj="Barclays-Enterprise-Scale"
-
-#Inspect environment security
-$baseurl = "https://dev.azure.com/$org/$proj"
-
-$url = "$baseurl/_environments/security"
- 
-Write-Output $url
