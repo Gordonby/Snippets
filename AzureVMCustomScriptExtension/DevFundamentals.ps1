2f56cf8f215f269723889b1198e774f6b09feaf4
Gordon Byers <gordon.byers@microsoft.com>
2023-02-24 13:23:35 +0000

    Update and rename fullDevelopmentSetup.ps1 to DevFundamentals.ps1

--- AzureVMCustomScriptExtension/DevFundamentals.ps1
+++ AzureVMCustomScriptExtension/DevFundamentals.ps1
@@ -1,28 +1,37 @@
+#Install chocolatey
 If ((Get-ExecutionPolicy) -ne 'RemoteSigned') {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force}
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
+#Install general apps
 choco install 7zip -y
 choco install adobereader -y
+choco install ffmpeg -y
+choco install Firefox -y
+choco install GoogleChrome -y
+choco install paint.net -y
+choco install pwsh -y
+
+#Install azure apps
 choco install azcopy10 -y
 choco install azure-cli -y
 choco install azure-iot-installer -y
 choco install AzureStorageExplorer -y
-choco install docker-desktop -y
-choco install ffmpeg -y
-choco install Firefox -y
+
+#Install cloud native apps
 choco install flux -y
+choco install kubernetes-cli -y
+choco install kubernetes-helm -y
+
+#Install core dev apps (not language specifc)
+choco install docker-desktop -y
 choco install gh -y
 choco install git -y
 choco install github-desktop -y
 choco install gnupg -y
-choco install GoogleChrome -y
 choco install jq -y
-choco install kubernetes-cli -y
-choco install kubernetes-helm -y
-choco install paint.net -y
-choco install pulumi -y
-choco install pwsh -y
 choco install servicebusexplorer -y
-choco install terraform -y
 choco install vscode -y
-choco install git -y
+
+#IaC
+choco install terraform -y
+choco install pulumi -y
