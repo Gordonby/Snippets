1887f90124b40bfae839fbb5b954b82f17908523
Gordon Byers <gordon.byers@microsoft.com>
2023-02-24 21:01:17 +0000

    Update DevFundamentalsUbu.sh

--- AzureVMCustomScriptExtension/DevFundamentalsUbu.sh
+++ AzureVMCustomScriptExtension/DevFundamentalsUbu.sh
@@ -3,6 +3,13 @@ sudo apt update && sudo apt upgrade
 #Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+#Install stuff with snap
+sudo apt install snapd
 sudo snap install powershell --classic
 sudo snap install helm --classic
 sudo snap install kubectl --classic
+
+#Install Node16
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
