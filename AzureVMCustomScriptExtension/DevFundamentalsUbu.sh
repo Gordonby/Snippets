3737aef9157b67ea771babeeeaede39e9002a06e
Gordon Byers <gordon.byers@microsoft.com>
2023-02-24 20:59:31 +0000

    Create DevFundamentalsUbu.sh

---
+++ AzureVMCustomScriptExtension/DevFundamentalsUbu.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt upgrade
+
+#Install Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+sudo snap install powershell --classic
+sudo snap install helm --classic
+sudo snap install kubectl --classic
