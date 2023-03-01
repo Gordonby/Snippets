e685ec526e8c326178bed1f3ec1e00f8c0340e42
Gordon Byers <gordon.byers@microsoft.com>
2023-03-01 17:22:37 +0000

    Update azureDeveloper.ps1

--- AzureVMCustomScriptExtension/azureDeveloper.ps1
+++ AzureVMCustomScriptExtension/azureDeveloper.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1\'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #Install azure apps
 choco install azcopy10 -y
