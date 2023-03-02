e8e3a5f92daf329e4c24a1091fb41d53658192fe
Gordon Byers <gordon.byers@microsoft.com>
2023-03-02 12:01:21 +0000

    Update winget-general.ps1

--- AzureVMCustomScriptExtension/winget-general.ps1
+++ AzureVMCustomScriptExtension/winget-general.ps1
@@ -1,2 +1,5 @@
 #Microsoft Remote Desktop 9WZDNCRFJ3PS
 winget install -e -h --id=9WZDNCRFJ3PS --source=msstore --accept-package-agreements
+
+#Netflix
+winget install -e -h --id=9WZDNCRFJ3TJ --source=msstore --accept-package-agreements
