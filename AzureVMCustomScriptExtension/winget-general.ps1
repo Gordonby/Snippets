4dcf17098a49c34417de678ea9a7eae370e7502f
Gordon Byers <gordon.byers@microsoft.com>
2023-03-02 12:00:28 +0000

    Update winget-general.ps1

--- AzureVMCustomScriptExtension/winget-general.ps1
+++ AzureVMCustomScriptExtension/winget-general.ps1
@@ -1,2 +1,2 @@
 #Microsoft Remote Desktop 9WZDNCRFJ3PS
-winget install -e -h --id=9WZDNCRFJ3TJ --source=msstore --accept-package-agreements
+winget install -e -h --id=9WZDNCRFJ3PS --source=msstore --accept-package-agreements
