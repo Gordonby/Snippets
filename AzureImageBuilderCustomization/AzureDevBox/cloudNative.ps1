cee0cf891f180982a74391e6de082bdbd4d00d2a
Gordon Byers <gordon.byers@microsoft.com>
2023-03-02 08:56:32 +0000

    Create cloudNative.ps1

---
+++ AzureImageBuilderCustomization/AzureDevBox/cloudNative.ps1
@@ -0,0 +1,8 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+#Install cloud native apps
+choco install flux -y
+choco install kubernetes-cli -y
+choco install kubernetes-helm -y
