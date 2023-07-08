6fcb7fe39e24677db6ccef921feb721594d45217
Gordon Byers <gordonbyers@gmail.com>
2023-07-08 10:07:59 +0100

    Update winget-general.ps1

--- AzureVMCustomScriptExtension/winget-general.ps1
+++ AzureVMCustomScriptExtension/winget-general.ps1
@@ -3,3 +3,6 @@ winget install -e -h --id=9WZDNCRFJ3PS --source=msstore --accept-package-agreeme
 
 #Netflix
 winget install -e -h --id=9WZDNCRFJ3TJ --source=msstore --accept-package-agreements
+
+#PowerBi Desktop
+winget install -e -h --id=9NTXR16HNW1T --source=msstore --accept-package-agreements
