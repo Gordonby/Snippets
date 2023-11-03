f80ec577988ef95b520c1455ba25370cfe6e697f
Gordon Byers <gordonbyers@gmail.com>
2023-11-03 07:44:16 +0000

    Update DevFundamentalsUbu.sh

--- AzureVMCustomScriptExtension/DevFundamentalsUbu.sh
+++ AzureVMCustomScriptExtension/DevFundamentalsUbu.sh
@@ -9,6 +9,10 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo apt-get install -y wget apt-transport-https software-properties-common
 source /etc/os-release
 wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
 
 #Install PowerShell Modules
 Install-Module -Name Az -Repository PSGallery -Force
